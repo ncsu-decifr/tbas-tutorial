b97e6505eabdd506619715ebee75ad25100faf94
jim <jbwhite2@ncsu.edu>
2020-01-22 15:38:07 -0500

    tutorial 4

--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,10 @@ gulp.task('move_images4', function() {
     return gulp.src("images/tbas-tutorial4/*.*")
         .pipe(gulp.dest('/var/www/html/tbas2_1/pages/images/tbas-tutorial4'));
 });
+gulp.task('move_images4a', function() {
+    return gulp.src("images/tbas-tutorial4a/*.*")
+        .pipe(gulp.dest('/var/www/html/tbas2_1/pages/images/tbas-tutorial4a'));
+});
 gulp.task('move_images1a', function() {
     return gulp.src("images/tbas-tutorial1a/*.*")
         .pipe(gulp.dest('/var/www/html/tbas2_1/pages/images/tbas-tutorial1a'));
@@ -72,6 +76,7 @@ gulp.task('default', [
     'move_images2',
     'move_images3',
     'move_images4',
+    'move_images4a',
     'move_images1a',
     'move_images2a',
     'move_images5',
